084400337ac9dd1027a7eac2ae0be269ed19f441
Craio98 <craioveanulm@gmail.com>
2023-12-01 21:09:07 +0200

    Adding Classes, including Constructors

---
+++ src/main/java/com/gentech/DatasetViewer/DatasetViewerApplication.java
@@ -0,0 +1,13 @@
+package com.gentech.DatasetViewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatasetViewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatasetViewerApplication.class, args);
+	}
+
+}
